77f20281af2fae33e4907b6af6ed42b756e925db
junaids79 <junaids5954@gmail.com>
2026-07-02 00:04:27 +0530

    layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
               reverseOrder={false}
             />
             {children}
+            <Analytics/>
             <Footer />
           </ThemeProvider>
         </body>
